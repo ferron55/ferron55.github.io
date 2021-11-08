a712f80b0e857ecd6e71fd1267b4049c35d6927d
ferron55 <91151231+ferron55@users.noreply.github.com>
2021-11-08 22:04:39 +0300

    update

---
+++ task4/script.js
@@ -0,0 +1,21 @@
+var names=new Array();
+names[0]="Yakoov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="Paul";
+names[5]="ferit";
+names[6]="Ally";
+names[7]="paula";
+names[8]="Laura";
+names[8]="jimmy";
+
+
+for (var i = 0; i <names.length; i++) {
+	if (names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+console.log("Goodbye "+ names[i])
+	}
+	else{
+console.log("Hello "+ names[i])
+	}
+} 
